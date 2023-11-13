fe0958dbc71deef771e9cec291dd53600d945078
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2023-11-13 15:46:47 +0100

    Chore: support css modules

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../styles/Dropdown.module.css'
+import * as styles from '../styles/Dropdown.module.css'
 
 export const Dropdown = (props) => {
     const [active, setActive] = React.useState(props.defaultSelected)
